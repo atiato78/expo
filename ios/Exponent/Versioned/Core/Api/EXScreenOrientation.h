a0c8101c51ac409e355d73ecbd454020c80ed9f6
Quinlan Jung <quin@expo.io>
2019-01-03 13:05:18 -0800

    typo

--- ios/Exponent/Versioned/Core/Api/EXScreenOrientation.h
+++ ios/Exponent/Versioned/Core/Api/EXScreenOrientation.h
@@ -1,7 +1,7 @@
 // Copyright 2015-present 650 Industries. All rights reserved.
 
 #import <React/RCTBridgeModule.h>
-#import <React/RCTConvert.h>s
+#import <React/RCTConvert.h>
 
 #import "EXScopedEventEmitter.h"
 
