20ca8313cc2cee280266c3355a53442905406cef
Evan Bacon <baconbrix@gmail.com>
2019-06-03 03:38:45 -0700

    [WIP]

--- apps/storybook/.storybook/config.js
+++ apps/storybook/.storybook/config.js
@@ -1,6 +1,6 @@
 import { withKnobs } from '@storybook/addon-knobs';
 import { addDecorator, addParameters, configure } from '@storybook/react';
-import { create } from '@storybook/theming';
+import { create, themes } from '@storybook/theming';
 import * as React from 'react';
 import requireContext from 'require-context.macro';
 
@@ -10,6 +10,46 @@ import centered from './decorator-centered';
 addDecorator(centered);
 
 // addDecorator(withKnobs({ escapeHTML: false, skipIfNoParametersOrOptions: true }));
+
+const colors = {
+  purple: '#4630EB',
+  secondary: 'rgba(0,0,0,0)',
+};
+const expoTheme = {
+  colorPrimary: colors.purple,
+  colorSecondary: colors.purple,
+
+  // UI
+  appBg: '#F3F3F3',
+  appContentBg: 'white',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: 'rgba(0, 0, 32, 0.9)',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+
+  // Toolbar default and active colors
+  barTextColor: 'rgba(0, 0, 32, 0.9)',
+  barSelectedColor: colors.purple,
+  barBg: 'white',
+
+  // Form colors
+  inputBg: '#FFFFFF',
+  inputBorder: 'rgba(0,0,0,.1)',
+  inputBorderRadius: 4,
+  inputTextColor: '#333333',
+
+  // Brand
+  brandTitle: 'Expo',
+  brandUrl: 'https://github.com/expo/expo/tree/master/apps/storybook',
+  gridCellSize: 12,
+  brandImage: require('../assets/brand-icon.png'),
+};
 
 addParameters({
   options: {
@@ -21,12 +61,7 @@ addParameters({
     hierarchySeparator: /\./,
     hierarchyRootSeparator: /\|/,
     enableShortcuts: true,
-    theme: create({
-      base: 'light',
-      brandTitle: 'Expo',
-      brandUrl: 'https://github.com/expo/expo/tree/master/apps/storybook',
-      gridCellSize: 12,
-    }),
+    theme: create(expoTheme),
   },
 });
 
