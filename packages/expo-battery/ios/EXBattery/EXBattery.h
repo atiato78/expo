61e89b6ffa66e772759a5a69ced9f84b34e4c3a6
Robert L <1998.robert@gmail.com>
2019-07-02 14:27:11 -0700

    [ios] Add battery methods

--- packages/expo-battery/ios/EXBattery/EXBattery.h
+++ packages/expo-battery/ios/EXBattery/EXBattery.h
@@ -3,6 +3,9 @@
 #import <UMCore/UMExportedModule.h>
 #import <UMCore/UMModuleRegistryConsumer.h>
 #import <Foundation/Foundation.h>
+#import <UMCore/UMEventEmitter.h>
+#import <UMCore/UMEventEmitterService.h>
 
-@interface EXBattery : UMExportedModule <UMModuleRegistryConsumer>
+@interface EXBattery : UMExportedModule <UMModuleRegistryConsumer, UMEventEmitter>
+
 @end
--- packages/expo-battery/ios/EXBattery/EXBattery.m
+++ packages/expo-battery/ios/EXBattery/EXBattery.m
@@ -6,6 +6,8 @@
 @interface EXBattery ()
 
 @property (nonatomic, weak) UMModuleRegistry *moduleRegistry;
+@property (nonatomic, weak) id <UMEventEmitterService> eventEmitter;
+@property (nonatomic, assign) BOOL hasListeners;
 
 @end
 
@@ -21,7 +23,150 @@ UM_EXPORT_MODULE(ExpoBattery);
 - (void)setModuleRegistry:(UMModuleRegistry *)moduleRegistry
 {
   _moduleRegistry = moduleRegistry;
+  _eventEmitter = [moduleRegistry getModuleImplementingProtocol:@protocol(UMEventEmitterService)];
 }
+
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[@"Expo.batteryLevelDidChange", @"Expo.powerStateDidChange", @"Expo.powerModeDidChange"];
+}
+
+- (void)startObserving {
+  _hasListeners = YES;
+}
+
+- (void)stopObserving {
+  _hasListeners = NO;
+}
+
+- (id)init
+{
+  if ((self = [super init])) {
+    [UIDevice.currentDevice setBatteryMonitoringEnabled:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(batteryLevelDidChange:)
+                                                 name:UIDeviceBatteryLevelDidChangeNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(powerStateDidChange:)
+                                                 name:UIDeviceBatteryStateDidChangeNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(powerModeDidChange:)
+                                                 name:NSProcessInfoPowerStateDidChangeNotification
+                                               object:nil];
+  }
+  
+  return self;
+}
+
+- (void)dealloc {
+  [self invalidate];
+}
+
+- (void)invalidate {
+  _eventEmitter = nil;
+  [UIDevice currentDevice].batteryMonitoringEnabled = NO;
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:UIDeviceBatteryLevelDidChangeNotification
+                                                object:nil];
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:UIDeviceBatteryStateDidChangeNotification
+                                                object:nil];
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:NSProcessInfoPowerStateDidChangeNotification
+                                                object:nil];
+}
+
+// Called at most once every minute
+- (void)batteryLevelDidChange:(NSNotification *)notification
+{
+  NSLog(@"batteryLevelDidChange was called");
+  if (!_hasListeners) {
+    NSLog(@"and hasListeners was false");
+    return;
+  }
+  
+  float batteryLevel = [[self getPowerState][@"batteryLevel"] floatValue];
+  [_eventEmitter sendEventWithName:@"Expo.batteryLevelDidChange" body:@(batteryLevel)];
+}
+
+- (void)powerStateDidChange:(NSNotification *)notification
+{
+  NSLog(@"powerStateDidChange was called");
+  if (!_hasListeners) {
+    NSLog(@"and hasListeners was false");
+    return;
+  }
+  
+  [_eventEmitter sendEventWithName:@"Expo.powerStateDidChange" body:[self getPowerState]];
+}
+
++ (NSString *)valueForIsLowPowerModeEnabled:(BOOL)isLowPowerModeEnabled
+{
+  return isLowPowerModeEnabled ? @"on" : @"off";
+}
+
+- (void)powerModeDidChange:(NSNotification *)notification
+{
+  if(!_hasListeners) {
+    return;
+  }
+  
+  [_eventEmitter sendEventWithName:@"Expo.powerModeDidChange" body:[EXBattery.class valueForIsLowPowerModeEnabled: NSProcessInfo.processInfo.isLowPowerModeEnabled]];
+}
+
+
+- (NSDictionary *)getPowerState
+{
+#if RCT_DEV && (!TARGET_IPHONE_SIMULATOR) && !TARGET_OS_TV
+  if ([UIDevice currentDevice].isBatteryMonitoringEnabled != YES) {
+    RCTLogWarn(@"Battery monitoring is not enabled. "
+               "You need to enable monitoring with `[UIDevice currentDevice].batteryMonitoringEnabled = TRUE`");
+  }
+#endif
+#if RCT_DEV && TARGET_IPHONE_SIMULATOR && !TARGET_OS_TV
+  if ([UIDevice currentDevice].batteryState == UIDeviceBatteryStateUnknown) {
+    RCTLogWarn(@"Battery state `unknown` and monitoring disabled, this is normal for simulators and tvOS.");
+  }
+#endif
+  
+  NSArray *batteryStates = @[@"unknown", @"unplugged", @"charging", @"full"];
+  __block NSDictionary *powerState;
+  
+  [UMUtilities performSynchronouslyOnMainThread:^{
+    powerState = @{
+                   @"batteryLevel": @([UIDevice currentDevice].batteryLevel),
+                   @"batteryState": batteryStates[[UIDevice currentDevice].batteryState],
+                   @"lowPowerMode": NSProcessInfo.processInfo.isLowPowerModeEnabled ? @"on" : @"off"
+                   };
+  }];
+  return powerState;
+}
+
+UM_EXPORT_METHOD_AS(getPowerStateAsync,
+                    getPowerStateAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
+{
+  resolve([self getPowerState]);
+}
+
+UM_EXPORT_METHOD_AS(getBatteryLevelAsync,
+                    getBatteryLevelAsyncWithResolver:(UMPromiseResolveBlock)resolve
+                    rejecter:(UMPromiseRejectBlock)reject)
+{
+  resolve(@([[self getPowerState][@"batteryLevel"] floatValue]));
+}
+
+UM_EXPORT_METHOD_AS(isBatteryChargingAsync,
+                    isBatteryCharingAsyncWithResolver:(UMPromiseResolveBlock)resolve rejecter:(UMPromiseRejectBlock)reject)
+{
+  BOOL isCharging = [[self getPowerState][@"batteryState"] isEqualToString:@"charging"];
+  resolve(@(isCharging));
+}
+
+
 
 UM_EXPORT_METHOD_AS(someGreatMethodAsync,
                     options:(NSDictionary *)options
