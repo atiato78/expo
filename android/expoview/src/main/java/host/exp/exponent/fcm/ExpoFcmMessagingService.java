7ebbd398d91b8a8f7dd44ad7811ea9a2ab8e9afe
szymon <szymon.kapala@swmansion.com>
2019-07-31 09:52:53 +0200

    small fix

--- android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
+++ android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
@@ -3,7 +3,7 @@ package host.exp.exponent.fcm;
 import android.os.Bundle;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.RemoteMessage;
 
 import host.exp.exponent.Constants;
 import host.exp.exponent.notifications.presenters.NotificationPresenterProvider;
@@ -41,15 +41,14 @@ public class ExpoFcmMessagingService extends FirebaseMessagingService {
     String experienceId = remoteMessage.getData().get(NOTIFICATION_EXPERIENCE_ID_KEY);
 
       bundle.putString(NOTIFICATION_EXPERIENCE_ID_KEY, experienceId);
-      bundle.putString(NOTIFICATION_CHANNEL_ID, msg.getString(NOTIFICATION_CHANNEL_ID));
-      bundle.putString(NOTIFICATION_BODY, msg.getString("message"));
-      bundle.putString(NOTIFICATION_TITLE, msg.getString(NOTIFICATION_TITLE));
-      bundle.putString(NOTIFICATION_CATEGORY, msg.getString(NOTIFICATION_CATEGORY));
-      bundle.putString(NOTIFICATION_ICON, msg.getString(NOTIFICATION_ICON));
-      bundle.putInt(NOTIFICATION_PRIORITY, msg.getInt(NOTIFICATION_PRIORITY));
-      bundle.putString(NOTIFICATION_COLOR, msg.getString(NOTIFICATION_COLOR));
-      bundle.putString(NOTIFICATION_DATA, msg.getString("body"));
-      bundle.putString(NOTIFICATION_SOUND, msg.getString(NOTIFICATION_SOUND));
+      bundle.putString(NOTIFICATION_CHANNEL_ID, remoteMessage.getData().get(NOTIFICATION_CHANNEL_ID));
+      bundle.putString(NOTIFICATION_BODY, remoteMessage.getData().get("message"));
+      bundle.putString(NOTIFICATION_TITLE, remoteMessage.getData().get(NOTIFICATION_TITLE));
+      bundle.putString(NOTIFICATION_CATEGORY, remoteMessage.getData().get(NOTIFICATION_CATEGORY));
+      bundle.putString(NOTIFICATION_ICON, remoteMessage.getData().get(NOTIFICATION_ICON));
+      bundle.putString(NOTIFICATION_COLOR, remoteMessage.getData().get(NOTIFICATION_COLOR));
+      bundle.putString(NOTIFICATION_DATA, remoteMessage.getData().get("body"));
+      bundle.putString(NOTIFICATION_SOUND, remoteMessage.getData().get(NOTIFICATION_SOUND));
       bundle.putBoolean(NOTIFICATION_REMOTE, true);
 
     NotificationPresenterProvider.getNotificationPresenter().presentNotification(
