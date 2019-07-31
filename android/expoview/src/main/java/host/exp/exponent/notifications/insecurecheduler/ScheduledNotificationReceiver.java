b6494c7b2d36f78e6ebde982c356a7123420aded
szymon <szymon.kapala@swmansion.com>
2019-07-31 14:06:03 +0200

    done

--- android/expoview/src/main/java/host/exp/exponent/notifications/insecurecheduler/ScheduledNotificationReceiver.java
+++ android/expoview/src/main/java/host/exp/exponent/notifications/insecurecheduler/ScheduledNotificationReceiver.java
@@ -1,4 +1,4 @@
-package host.exp.exponent.notifications;
+package host.exp.exponent.notifications.insecurecheduler;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -12,9 +12,12 @@ import android.os.Bundle;
 import host.exp.exponent.ExponentManifest;
 import host.exp.exponent.di.NativeModuleDepsProvider;
 import host.exp.exponent.kernel.KernelConstants;
+import host.exp.exponent.notifications.NotificationHelper;
 import host.exp.exponent.notifications.managers.SchedulersManagerProxy;
 import host.exp.exponent.notifications.helpers.Utils;
 import host.exp.exponent.notifications.presenters.NotificationPresenterProvider;
+
+import static host.exp.exponent.notifications.NotificationConstants.NOTIFICATION_EXPERIENCE_ID_KEY;
 
 public class ScheduledNotificationReceiver extends BroadcastReceiver {
 
@@ -30,6 +33,8 @@ public class ScheduledNotificationReceiver extends BroadcastReceiver {
     HashMap details = (HashMap) bundle.getSerializable(KernelConstants.NOTIFICATION_OBJECT_KEY);
     int notificationId = bundle.getInt(KernelConstants.NOTIFICATION_ID_KEY, 0);
     String schedulerId = (String) details.get(SchedulersManagerProxy.SCHEDULER_ID);
+
+    ThreadSafeInsecureScheduler.getInstance().cancelScheduled(NOTIFICATION_EXPERIENCE_ID_KEY, notificationId, context);
 
     SchedulersManagerProxy.getInstance(context).rescheduleOrDelete(schedulerId);
 
