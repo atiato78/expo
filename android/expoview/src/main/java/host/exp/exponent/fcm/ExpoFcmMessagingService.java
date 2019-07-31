c0c840cfc35b02c80bf37a5a2cb5f6606c8d1975
szymon <szymon.kapala@swmansion.com>
2019-07-31 09:47:08 +0200

    changed the way of parsing Firebase message

--- android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
+++ android/expoview/src/main/java/host/exp/exponent/fcm/ExpoFcmMessagingService.java
@@ -3,10 +3,7 @@ package host.exp.exponent.fcm;
 import android.os.Bundle;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.firebase.messaging.RemoteMessage
 
 import host.exp.exponent.Constants;
 import host.exp.exponent.notifications.presenters.NotificationPresenterProvider;
@@ -43,22 +40,17 @@ public class ExpoFcmMessagingService extends FirebaseMessagingService {
     Bundle bundle = new Bundle();
     String experienceId = remoteMessage.getData().get(NOTIFICATION_EXPERIENCE_ID_KEY);
 
-    try {
-      JSONObject msg = new JSONObject(remoteMessage.getData().get("newFormat"));
       bundle.putString(NOTIFICATION_EXPERIENCE_ID_KEY, experienceId);
       bundle.putString(NOTIFICATION_CHANNEL_ID, msg.getString(NOTIFICATION_CHANNEL_ID));
-      bundle.putString(NOTIFICATION_BODY, msg.getString(NOTIFICATION_BODY));
+      bundle.putString(NOTIFICATION_BODY, msg.getString("message"));
       bundle.putString(NOTIFICATION_TITLE, msg.getString(NOTIFICATION_TITLE));
       bundle.putString(NOTIFICATION_CATEGORY, msg.getString(NOTIFICATION_CATEGORY));
       bundle.putString(NOTIFICATION_ICON, msg.getString(NOTIFICATION_ICON));
       bundle.putInt(NOTIFICATION_PRIORITY, msg.getInt(NOTIFICATION_PRIORITY));
       bundle.putString(NOTIFICATION_COLOR, msg.getString(NOTIFICATION_COLOR));
-      bundle.putString(NOTIFICATION_DATA, msg.getString(NOTIFICATION_DATA));
+      bundle.putString(NOTIFICATION_DATA, msg.getString("body"));
       bundle.putString(NOTIFICATION_SOUND, msg.getString(NOTIFICATION_SOUND));
       bundle.putBoolean(NOTIFICATION_REMOTE, true);
-    } catch (JSONException e) {
-      e.printStackTrace();
-    }
 
     NotificationPresenterProvider.getNotificationPresenter().presentNotification(
       this.getApplicationContext(),
